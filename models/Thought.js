229f053fbdb5d84f9477c5b8a16cc940ea5f5951
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-02-19 19:00:38 -0500

    4.ADDED thought schema

---
+++ models/Thought.js
@@ -0,0 +1,50 @@
+const {Schema, model} = require('mongoose')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText:{
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+            get: function(value) {
+                // Format the createdAt date as year, month, day, and military time
+                const date = new Date(value);
+                const year = date.getFullYear();
+                const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
+                const day = date.getDate().toString().padStart(2, '0');
+                const hours = date.getHours().toString().padStart(2, '0');
+                const minutes = date.getMinutes().toString().padStart(2, '0');
+                return `${year}-${month}-${day} ${hours}:${minutes}`;
+            }
+        },
+        username:{
+            type: String,
+            required: true,
+        },
+        reactions:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Reaction'
+            }
+        ]
+    },
+    {
+        toJSON:{
+            getters:true,
+            virtuals: true
+        },
+        id: false
+    }
+)
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema)
+
+module.exports = Thought
